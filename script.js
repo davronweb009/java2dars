0c513ad2853b8db7a8639458af32ad6e30d04cc6
davronweb009 <davronweb07@mail.ru>
2025-01-10 17:08:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let user = prompt("String typedagi malumot kiriting:");
+let user2 = prompt("Number typedagi malumot kiriting:");
+let user3 = prompt("boolean typedagi malumot kiriting:");
+let message = "hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?"
+alert(message)
+alert(
+  `Siz kiritgan ${user} so'zi  typega tegishli 
+  Siz kiritgan ${user2} so'zi  typega tegishli
+  Siz kiritgan ${user3} so'zi typega tegishli`
+);
